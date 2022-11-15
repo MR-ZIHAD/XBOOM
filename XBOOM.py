7b36cb37e718e6d33757fdb024043858cd4234b4
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2022-11-15 23:30:46 +0600

    Update XBOOM.py

--- XBOOM.py
+++ XBOOM.py
@@ -24,10 +24,10 @@ def update():
     try:
         toolVersion = open("./.version", "r").read()
     except:
-        toolVersion = "MR-ZIHAD"
+        toolVersion = "MRZIHAD"
     
     try:
-        mainVersion = requests.get("https://raw.githubusercontent.com/MR-ZIHAD/XBOOM/main/.version").text
+        mainVersion = requests.get("https://github.com/MR-ZIHAD/XBOOM/main/.version").text
     except:
         psb("\n\033[92m    [\033[91m!\033[92m] Please Connect To The Internet!")
         time.sleep(1)
