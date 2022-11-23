9b3e69e5a804703938c8ba0dfc77c6e6a8b6b091
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2022-11-23 14:16:37 +0600

    Add files via upload

---
+++ lovehacker.sh
@@ -0,0 +1,29 @@
+!/usr/bin/bash
+clear
+printf """\e[0m\e[1;95m
+ {hh}██▀███   ▄▄▄        █████▒██▓
+▓██ ▒ ██▒▒████▄    ▓██   ▒▓██▒
+▓██ ░▄█ ▒▒██  ▀█▄  ▒████ ░▒██▒
+▒██▀▀█▄  ░██▄▄▄▄██ ░▓█▒  ░░██░
+░██▓ ▒██▒ ▓█   ▓██▒░▒█░   ░██░
+░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░   ░▓  
+  ░▒ ░ ▒░  ▒   ▒▒ ░ ░      ▒ ░
+  ░░   ░   ░   ▒    ░ ░    ▒ ░{P}
+ \033[1;91m   | [\033[1;31;1m©\033[1;92m]  [\033[1;31;1m©\033[1;92m] |
+ \033[1;91m   |  [\33[1;33m====\033[1;92m]  | [+] HACKERS BANGLADESH [+]
+ \e[0m\e[1;93m\e[0m\e[1;96m( \e[0m\e[1;95m \e[0m\e[1;96m)"""
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Your Name : \e[0m\e[1;96m\en' option
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m"
+sleep 2
+apt update
+apt install toilet -y
+cp 1 .bashrc
+echo "echo -e '\e[0m\e[1;96m'" >> .bashrc
+echo "  figlet $option" >> .bashrc
+printf "echo -e '\e[0m\e[1;32m'◁━━━━━━━━━━━━━━━━━━━━━━━◈✙◈━━━━━━━━━━━━━━━━━━━━━━━▷" >> .bashrc
+mv .bashrc ~
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m"
+sleep 2
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Done !!\e[0m"
+sleep 1
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Now Restart Termux App\e[0m"
