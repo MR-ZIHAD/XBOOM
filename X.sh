c747602dca1d8ebcbdbb49f5e78a4eb33222f2b0
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2022-11-27 00:39:18 +0600

    Create X.sh

---
+++ X.sh
@@ -0,0 +1,199 @@
+# Decrypted by RAFI
+# Github- https://github.com/MR-ZIHAD/
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+echo
+echo $i"╔═╗┬ ┬┌┬┐┌─┐  ┬─┐┌─┐┌─┐┌─┐┬─┐┌┬┐      "
+echo $i"╠═╣│ │ │ │ │  ├┬┘├┤ ├─┘│ │├┬┘ │          "
+echo $i"╩ ╩└─┘ ┴ └─┘  ┴└─└─┘┴  └─┘┴└─ ┴       "
+echo $i""$bi"●"$me"═━═━═━═━═━═━═━═━═━═"$i"❴"$i"RAFI"$i"❵"$i"═━═━═══━═━═━━══━═━═"$i"●   "
+echo $i"[+]"$i" Author      "$i"    : "$i"ZIHAD HOSSAIN RAFI "
+echo $i"[+] "$i"Guthub         "$i" :"$i" MR-ZIHAD    "
+echo $i"[+] "$i"version       "$i"  :"$i" 0.1"
+echo $i"[+] "$i"TEAM          "$i"  : "$i"CYBER"$pu" STRIKER  "$bi"TEAM " 
+echo $i"[+] "$i"Tooles  Name    "$i": "$i"AUTO "$bi"REPORT"
+echo $i"[+] "$i"YOUTUBE CONTACT "$i": "$i"TECHNICAL ZIHAD "
+echo $i"[+] "$i"FACEBOOK CONTACT"$i": "$i"zihad.hossain36 "
+echo $i""$i"●"$i"══━═━═━═━━═══━═━═━═"$i"❴"$pu"RAFI"$bi"❵"$me"═━═━═══━═━═━═━═━═━═"$i"●   "
+echo $i"|"$i" 1"$i" |"$i" FACEBOOK AUTO REPORT WORK     "
+echo $i"|"$i" 2"$i" |"$i" TERMUX SET-UPS    "
+echo $i"|"$i" 3"$i" |"$i" EXIT    "
+echo $i""$i"●"$i"══━═━═━═━━═══━═━═━═"$i"❴"$i"RAFI"$i"❵"$i"═━═━═══━═━═━═━═━═━═"$i"●   "
+echo
+echo $me"┌==="$i"["$i"ZIHAD"$i"]"$i"===="$i"["$i""HOSSAIN""$i"]"$i"===="$i"["$i""RAFI""$i"]"
+echo $me"¦"
+read -p"└──➣➢➣ " pil
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+if [ $pil = 2 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U C C S E S S "|lolcat
+fi
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"TERMUX USE TO THANKS 👍"
+sleep 2
+echo $i""
+sleep 2
+echo $ku"FACEBOOK :"$i" https://www.facebook.com/zihad.hossain36"
+echo $ku"Facebook :"$i" ZIHAD HOSSAIN RAFI"
+sleep 2
+echo $pur">> THANK MY TOOL  USE  🧨��🧚✨🔥<<"
+exit
+fi
