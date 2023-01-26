ae97c22e2040d7b458b9051b0fa07fc8debffb20
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2023-01-26 08:25:38 +0600

    Update bruteX.py

--- bruteX.py
+++ bruteX.py
@@ -115,7 +115,7 @@ def hackbook():
 	if sys.version_info[0] !=3: 
 		sys.exit()
 	
-	post_url='https://p.facebook.com/login.php'
+	post_url='https://m.facebook.com/login.php'
 	headers = {
 		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 	}
